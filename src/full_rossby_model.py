Found that windspharm reverses latitude in its calculation (at least sometimes). Fixed plots and analytical code to turn plots and calcs right side up again. Continous experimentation with full_rossby_model - subtracting LT zonal mean zonal wind (equivalent to subtracting phase shift) makes zeros match the gyre oscillation, but c_phase values are opposite to what I expect (still).

src/full_rossby_model.py
@@ -6,6 +6,7 @@ Created on Mon Feb 13 16:32:45 2023
 @author: Mo Cohen
 """
 import iris, windspharm
+import warnings
 from windspharm.iris import VectorWind
 import iris.plot as iplt
 import numpy as np
@@ -16,6 +17,7 @@ from iris.analysis import calculus
 from numpy import unravel_index
 
 period = (506, 526)
+warnings.filterwarnings('ignore')
 
 def extract_core(winds, time_slice=-1, level=8):
    
@@ -29,7 +31,7 @@ def extract_core(winds, time_slice=-1, level=8):
 
 
 def model_rwave(cubes,startlon=30,start=218,end=250,nlat=90,nlon=144,level=8,
-                omega=1.19e-05,g=9.12,radius=5797818,lat=8,meaning=5,save='no'):
+                omega=1.19e-05,g=9.12,radius=5797818,lat=80,meaning=5,save='no'):
     
     
     for cube in cubes:
@@ -54,40 +56,41 @@ def model_rwave(cubes,startlon=30,start=218,end=250,nlat=90,nlon=144,level=8,
     winds = windspharm.iris.VectorWind(x_wind[:,level,:,:],y_wind[:,level,:,:])
     # Create a VectorWind data object from the x and y wind cubes
     rel_vort = winds.vorticity() 
+    rel_vort = np.flip(rel_vort.data,axis=1)
     
     core_lats = []
     core_lons = []
     for time in range(0,rel_vort.shape[0]):
-        rossby_core = unravel_index(np.argmax(rel_vort[time,:20,0:72].data, axis=None), rel_vort[time,:20,0:72].shape)
-#        print(rossby_core[0], rossby_core[1])
+        rossby_core = unravel_index(np.argmax(rel_vort[time,70:,0:72], axis=None), rel_vort[time,70:,0:72].shape)
+        print(rossby_core[0]+70, rossby_core[1])
 #        print(rel_vort[time,rossby_core[0],rossby_core[1]].data)   
-        core_lats.append(rossby_core[0])
+        core_lats.append(rossby_core[0]+70)
         core_lons.append(rossby_core[1])
     
 #    lat_deg = [np.abs(int(latitudes[item])) for item in core_lats] # Convert input row number to latitude in degrees north
     lon_deg = [int(longitudes[item]) for item in core_lons]
     lon_deg_meaned = np.convolve(np.array(lon_deg).flatten(),np.ones(meaning),'valid')/meaning
+    lon_diff = np.diff(lon_deg_meaned)
     
     lat_deg = int(latitudes[lat])*np.ones_like(core_lats)
 #    print(lat_deg)
     
+    shortterm_zmzw = x_wind[:,level,:,:].collapsed('longitude',iris.analysis.MEAN)
+    shortterm_zmzw = shortterm_zmzw.data
+    longterm_zmzw = longterm_x_wind[:,level,:,:].collapsed(['longitude','time'], iris.analysis.MEAN)
+    longterm_zmzw = longterm_zmzw.data
     
-#    lt_zmzw = []
+    lt_zmzw = []
     zmzw = []
+    
     for time in range(0, len(core_lats)):
-        row = core_lats[time]
-        shortterm_zmzw = x_wind[time,level,lat,0:72].collapsed('longitude',iris.analysis.MEAN)
-#        longterm_zmzw = longterm_x_wind[:,level,row,0:72].collapsed(['longitude','time'], iris.analysis.MEAN)
-
-        shortterm_zmzw = float(shortterm_zmzw.data)
-#        longterm_zmzw = float(longterm_zmzw.data)
-        
-        zmzw.append(shortterm_zmzw)
-#        lt_zmzw.append(longterm_zmzw)
+        row = core_lats[time]        
+        zmzw.append(shortterm_zmzw[time,row])
+        lt_zmzw.append(longterm_zmzw[row])
     
     lat_rad = np.array(lat_deg)*(np.pi/180) # Convert input latitude to radians
     beta = 2*omega*np.cos(lat_rad)/radius # Beta factor    
-    circum = 2*np.pi*radius*np.cos(lat_rad) # Circumference in meters at input latitude   
+    circum = 2*np.pi*radius*np.cos(lat_rad) # Circumference in meters at input latitude 
     x_num = 2*np.pi/circum
     
     d_theta = iris.analysis.calculus.differentiate(theta, 'level_height')
@@ -99,13 +102,14 @@ def model_rwave(cubes,startlon=30,start=218,end=250,nlat=90,nlon=144,level=8,
         Ld = (bv_freq[time,level,lat]*6800)/(2*omega*np.sin(lat_rad[time]))
         Ld_lat.append(Ld)
         
-    wave_num = beta + np.array(zmzw)*((1./np.array(Ld_lat))**2)
+    wave_num = beta + (np.array(zmzw) - np.array(lt_zmzw))*((1./np.array(Ld_lat))**2)
     wave_denom = x_num**2 + (1./np.array(Ld_lat))**2
     wave_component = wave_num/wave_denom
     
-    c_phase = (np.array(zmzw)) - wave_component 
+    c_phase = (np.array(zmzw) - np.array(lt_zmzw)) - wave_component 
     c_phase_meaned = np.convolve(c_phase.flatten(),np.ones(meaning),'valid')/meaning
     zero_ind = np.where(np.diff(np.sign(c_phase_meaned)))[0]
+
 
     zeroes = []
     for zc in zero_ind:
@@ -116,13 +120,17 @@ def model_rwave(cubes,startlon=30,start=218,end=250,nlat=90,nlon=144,level=8,
         interpolated_zero = t1 + (0-p1)* ((t2-t1)/(p2-p1))
         zeroes.append(interpolated_zero)
                             
-    print(zeroes)
     distance = np.cumsum(c_phase*60*60*24*(360/circum)) # Convert m/s to m/day = distance travelled in a day
+    deriv = lon_diff*(1/(60*60*24))*(circum[0]/360)
+    print(lon_diff)
+    print(circum[0])
+    print(deriv)
     
     fig, ax1 = plt.subplots()
     ax1.set_xlabel('Time [days]')
     ax1.set_ylabel('Velocity [m/s]')
     ax1.plot(time_axis,c_phase_meaned, color='b', label='Phase vel')
+    ax1.plot(time_axis[:-1], deriv, color='g', label='Deriv')
     ax1.plot([item+start for item in zeroes], np.zeros_like(zeroes), 'o', color='r')
     ax1.tick_params(axis='y', labelcolor='b')
     
@@ -152,7 +160,7 @@ def model_rwave(cubes,startlon=30,start=218,end=250,nlat=90,nlon=144,level=8,
     plt.plot(time_axis, lon_deg_meaned)
 #    plt.plot([item+500 for item in zeroes], [lon_deg[item] for item in zeroes], 'o', color='r')
 #    plt.plot(time_axis, lon_deg,'o', color='r', markevery=markers_on)
-    plt.title('Path travelled by southeast gyre')
+    plt.title('Path travelled by northeast gyre')
     plt.xlabel('Time [days]')
     plt.ylabel('Longitude [deg E]')
     plt.show()

src/vorticity.py
@@ -87,12 +87,15 @@ def plot_vorts(cubes, time_slice=-1, level=8, omega=1.19e-05, g=9.12, lat=50):
     # rel_vort = eddies.vorticity()
 
     rel_vort = winds.vorticity()
+    rel_vort = np.flip(rel_vort.data,axis=1)
+    # rel_vort = rel_vort.data
+    
 #    abs_vort = winds.absolutevorticity()
     # Calculate relative and absolute vorticity
     
-    f = np.full_like(rel_vort, f_constant) 
+    # f = np.full_like(rel_vort, f_constant) 
     # Make an array of f to add to relative vorticity
-    pot_vort = -g*(rel_vort[0:-1,:,:].data + f)*dth_dp.data
+    # pot_vort = -g*(rel_vort[0:-1,:,:] + f)*dth_dp.data
     # Calculate potential vorticity
     
     # Can quickly look at relative and absolute vorticity below if you want
@@ -101,7 +104,8 @@ def plot_vorts(cubes, time_slice=-1, level=8, omega=1.19e-05, g=9.12, lat=50):
     rexponent = -5
     plt.figure(figsize=(8,5))
     plt.contourf(np.arange(-longitudes/2, longitudes/2), np.arange(-latitudes/2, latitudes/2),
-                 np.roll(rel_vort[level,:,:,].data*r_constant, 72, axis=1),levels=rcolor_levs, cmap=redblu, norm=TwoSlopeNorm(0))
+                 np.roll(rel_vort[level,:,:,]*r_constant, 72, axis=1),levels=rcolor_levs, cmap=redblu, norm=TwoSlopeNorm(0))
+    # plt.gca().invert_yaxis()
     plt.title('Relative Vorticity, h=%s km, day=%s' %(heights[level], time_slice))
     plt.xlabel('Longitude [degrees]')
     plt.ylabel('Latitude [degrees]')
@@ -115,10 +119,10 @@ def plot_vorts(cubes, time_slice=-1, level=8, omega=1.19e-05, g=9.12, lat=50):
     plt.show()
     
     
-    rv_min = unravel_index(np.argmax(rel_vort[level,:20,0:72].data, axis=None), rel_vort[level,:20,0:72].shape)
-    print(rv_min[0], rv_min[1])
-    print(x_wind.coord('latitude').points[rv_min[0]], x_wind.coord('longitude').points[rv_min[1]])
-    print(rel_vort[level,rv_min[0],rv_min[1]].data)
+    rv_min = unravel_index(np.argmax(rel_vort[level,70:,0:72], axis=None), rel_vort[level,70:,0:72].shape)
+    print(rv_min[0]+70, rv_min[1])
+    print(x_wind.coord('latitude').points[rv_min[0]+70], x_wind.coord('longitude').points[rv_min[1]])
+    print(rel_vort[level,rv_min[0]+70,rv_min[1]])
     
     # iplt.contourf(abs_vort[level,:,:], redblu.N, cmap=redblu, norm=TwoSlopeNorm(0))
     # plt.title('Absolute Vorticity, h = %s km' %(heights[level]), y=1.20)